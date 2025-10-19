5c2871d5c860b229654b37bfbaf0fe688194615a
antikirra <antikirra@gmail.com>
2025-10-19 12:09:25 +0700

    refactor: improve uuid4 generation and validation logic

--- uuid4.php
+++ uuid4.php
@@ -9,11 +9,12 @@ use InvalidArgumentException;
  */
 function uuid4()
 {
-    $hex = bin2hex(\random_bytes(18));
-    $hex[8] = $hex[13] = $hex[18] = $hex[23] = '-';
-    $hex[14] = '4';
+    $data = \random_bytes(16);
 
-    return $hex;
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
 
 
@@ -24,12 +25,8 @@ function uuid4()
 function uuid4_validate($uuid4)
 {
     if (!is_string($uuid4)) {
-        throw new InvalidArgumentException('UUID must be a string, given: ' . gettype($uuid4));
+        throw new InvalidArgumentException('UUID must be a string, got ' . gettype($uuid4));
     }
 
-    if (strlen($uuid4) !== 36) {
-        return false;
-    }
-
-    return (bool)preg_match('~^[a-f\d]{8}-[a-f\d]{4}-4[a-f\d]{3}-[a-f\d]{4}-[a-f\d]{12}$~', $uuid4);
+    return preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i', $uuid4) === 1;
 }
